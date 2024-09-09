2b11bb4e7e6ad7027c4ff2648fd06a396729867c
rahul <rahulpatidar2629@gmail.com>
2024-09-09 15:22:44 +0530

    update code

--- chatApp_client_Server/backend/middlewere/verifyToken.js
+++ chatApp_client_Server/backend/middlewere/verifyToken.js
@@ -1,33 +1,11 @@
 const jsonwebtoken = require('jsonwebtoken');
 const SECRET_KEY = 'your_chat_app';
 
-// async function verifyToken(req, res, next) {
-//     var token = req.headers.authorization;
-//     if (token && token.startsWith('Bearer')) {
-//         token = token.split(' ')[1];
-//         try {
-//             const verified = jsonwebtoken.verify(token, SECRET_KEY);
-//             req.user = verified;
-//             next();
-//         }
-//         catch (error) {
-//             return res.status(401).json({
-//                 message: 'Token is not valid'
-//             })
-//         }
-//     }
-// },
-
-// const jsonwebtoken = require('jsonwebtoken');
-// const SECRET_KEY = 'your-secret-key';
-
-// Assuming cookie-parser is already configured in your app
 async function verifyToken(req, res, next) {
     // First, check the Authorization header for the token
     let token = req.headers.authorization;
 
     if (req.cookies && req.cookies.token) {
-        // If no Authorization header, check the cookies for a token
         token = req.cookies.token;
     } else if (token && token.startsWith('Bearer')) {
         token = token.split(' ')[1]; // Extract the token part
